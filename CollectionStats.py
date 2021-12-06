96eb0fcb47871ffb4ea08732b88109bf8c0618a0
spydrphi <spydrphi@gmail.com>
2021-12-06 09:06:34 -0500

    Update CollectionStats.py
    Added support for API key

--- CollectionStats.py
+++ CollectionStats.py
@@ -2,14 +2,23 @@
 
 import json
 import urllib.request
-
+import urllib.parse
 
 with open('wallet_addy.txt') as address_file:
 	wallet_addy = address_file.read().rstrip()
+	
+with open('api_key.txt') as key_file:
+	api_key = key_file.read().rstrip()
 
-hres = urllib.request.urlopen('https://api.opensea.io/api/v1/collections?asset_owner=' + wallet_addy + '&offset=0&limit=300')
+url = 'https://api.opensea.io/api/v1/collections?asset_owner=' + wallet_addy + '&offset=0&limit=300'
+headers = {'x-api-key': '{key}'.format(key=api_key)}
+
+req = urllib.request.Request(url, data=None, headers=headers)
+hres = urllib.request.urlopen(req)
+
 jsondata = json.loads(hres.read().decode("utf-8"))
 
+#debug statement, uncomment if needed
 #print(json.dumps(jsondata, indent=4, sort_keys=True))
 
 for data in jsondata:
